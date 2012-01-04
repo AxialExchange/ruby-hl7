397e1467b95b4f7b2de3b27eab3f3dc5b864fde4
Jon Kern <jonkern@comcast.net>
2012-01-04 18:33:32 -0500

    Added NK1 - Next of Kin segment plus Extended Telecom Notation and Extended Organization data type support

--- lib/segments/data_types.rb
+++ lib/segments/data_types.rb
@@ -1,3 +1,5 @@
+# Sometimes you can find helpful tips looking at implementations like here:
+# http://hl7api.sourceforge.net/apidocs/ca/uhn/hl7v2/model/v25/datatype/package-summary.html
 module HL7::Message::DataTypes
   attr_reader :XPN      #Extended Person Name
   attr_reader :XAD      #Extended Address
@@ -7,6 +9,8 @@ module HL7::Message::DataTypes
   attr_reader :CX       #Extended Composite ID with Check Digit
   attr_reader :PL       #Place
   attr_reader :CE       #Coded Element
+  attr_reader :XTN      #Extended telecommunication number
+  attr_reader :XON      #Extended Organization Name
 
   def self.XPN #for HL7 v2.3
     ["family_name", "given_name", "middle_name", "suffix", "prefix", "degree", "name_type_code", "name_representation_code"]
@@ -38,6 +42,16 @@ module HL7::Message::DataTypes
 
   def self.CE
     ["identifier", "text", "name_of_coding_system", "alternate_identifier", "alternate_text", "name_of_alternate_coding_system"]
+  end
+
+  def self.XTN
+    ["telephone_number", "use_code", "equipment_type", "email_address", "country_code", "area_or_city_code",
+     "local_number", "extension", "any_text", "extension_prefix", "speed_dial_code", "unformatted_number"]
+  end
+
+  def self.XON
+    ["organization_name", "organization_name_type_code", "id_number", "check_digit", "check_digit_scheme", "assigning_authority",
+     "identifier_type_code", "assigning_facility", "name_representation_code", "organization_identifier"]
   end
 
 end
