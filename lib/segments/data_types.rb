a3633a92fdb1cb0b420b002879c289b0e72b53c9
Guangming Cui <gcui@axialexchange.com>
2012-03-02 22:31:43 -0500

    Added more fields to HL7 data type CX

--- lib/segments/data_types.rb
+++ lib/segments/data_types.rb
@@ -13,45 +13,133 @@ module HL7::Message::DataTypes
   attr_reader :XON      #Extended Organization Name
 
   def self.XPN #for HL7 v2.3
-    ["family_name", "given_name", "middle_name", "suffix", "prefix", "degree", "name_type_code", "name_representation_code"]
+    ["family_name",
+     "given_name",
+     "middle_name",
+     "suffix",
+     "prefix",
+     "degree",
+     "name_type_code",
+     "name_representation_code"]
   end
 
   def self.XAD #for HL7 v2.3
-    ["street_address", "other_designation", "city", "state_province", "zip_postal_code", "country", "address_type", "other_geographic_designation", "county_parish", "census_tract", "address_representation_cod"]
+    ["street_address",
+     "other_designation",
+     "city",
+     "state_province",
+     "zip_postal_code",
+     "country",
+     "address_type",
+     "other_geographic_designation",
+     "county_parish",
+     "census_tract",
+     "address_representation_cod"]
   end
 
   def self.CN
-    ["id_number", "family_name", "given_name", "middle_name", "suffix", "prefix", "degree", "source_table", "assigning_authority"]
+    ["id_number",
+     "family_name",
+     "given_name",
+     "middle_name",
+     "suffix",
+     "prefix",
+     "degree",
+     "source_table",
+     "assigning_authority"]
   end
 
   def self.XCN #for HL7 v2.3
-    ["id_number", "family_name", "given_name", "middle_name", "suffix", "prefix", "degree", "source_table", "assigning_authority", "name_type_code", "id_check_digit", "check_digit_scheme", "id_type_code", "assigning_facility", "name_representation_code", "name_context", "name_validity_range", "name_assembly_order", "effective_date", "expiration_date", "professional_suffix", "assigning_jurisdiction", "assigning_agency_or_department"]
+    ["id_number",
+     "family_name",
+     "given_name",
+     "middle_name",
+     "suffix",
+     "prefix",
+     "degree",
+     "source_table",
+     "assigning_authority",
+     "name_type_code",
+     "id_check_digit",
+     "check_digit_scheme",
+     "id_type_code",
+     "assigning_facility",
+     "name_representation_code",
+     "name_context",
+     "name_validity_range",
+     "name_assembly_order",
+     "effective_date",
+     "expiration_date",
+     "professional_suffix",
+     "assigning_jurisdiction",
+     "assigning_agency_or_department"]
   end
 
   def self.MSG_TYPE
-    ["message_type", "trigger_event"]
+    ["message_type",
+     "trigger_event"]
   end
 
   def self.CX
-    ["id", "check_digit", "code_scheme", "assigning_authority", "id_type_code", "assigning_facility"]
+    ["id",
+     "check_digit",
+     "code_scheme",
+     "assigning_authority",
+     "id_type_code",
+     "assigning_facility",
+     "effective_date",
+     "expiration_date",
+     "assigning_jurisdiction",
+     "assigning_agency_or_department"]
   end
 
   def self.PL
-    ["point_of_care", "room", "bed", "facility", "location_status", "person_location_type", "building", "floor", "location_description"]
+    ["point_of_care",
+     "room",
+     "bed",
+     "facility",
+     "location_status",
+     "person_location_type",
+     "building",
+     "floor",
+     "location_description"]
   end
 
   def self.CE
-    ["identifier", "text", "name_of_coding_system", "alternate_identifier", "alternate_text", "name_of_alternate_coding_system"]
+    ["identifier",
+     "text",
+     "name_of_coding_system",
+     "alternate_identifier",
+     "alternate_text",
+     "name_of_alternate_coding_system"]
   end
 
   def self.XTN
-    ["telephone_number", "use_code", "equipment_type", "email_address", "country_code", "area_or_city_code",
-     "local_number", "extension", "any_text", "extension_prefix", "speed_dial_code", "unformatted_number"]
+    ["telephone_number",
+     "use_code",
+     "equipment_type",
+     "email_address",
+     "country_code",
+     "area_or_city_code",
+     "local_number",
+     "extension",
+     "any_text",
+     "extension_prefix",
+     "speed_dial_code",
+     "unformatted_number"]
   end
 
   def self.XON
-    ["organization_name", "organization_name_type_code", "id_number", "check_digit", "check_digit_scheme", "assigning_authority",
-     "identifier_type_code", "assigning_facility", "name_representation_code", "organization_identifier"]
+    ["organization_name",
+     "organization_name_type_code",
+     "id_number",
+     "check_digit",
+     "check_digit_scheme",
+     "assigning_authority",
+     "identifier_type_code",
+     "assigning_facility",
+     "name_representation_code",
+     "organization_identifier"]
   end
 
 end
