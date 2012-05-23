0ee94b36e556b256cc4926b4ee1509f81de4143a
Jon Kern <jonkern@comcast.net>
2012-05-23 16:51:15 -0400

    added sample zed segment

--- lib/segments/zed.rb
+++ lib/segments/zed.rb
@@ -1,4 +1,6 @@
 # encoding: UTF-8
+# Sample:
+#   ZED|ABSCESS^Abscess|3C1-12 Cardiology|11108232
 class HL7::Message::Segment::ZED < HL7::Message::Segment
   add_field :unknown1
   add_field :unknown2
