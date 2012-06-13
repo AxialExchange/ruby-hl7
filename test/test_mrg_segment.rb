fa4c48d25a4a35879e0b62605313ef059b792796
Guangming Cui <gcui@axialexchange.com>
2012-06-12 21:13:41 -0400

    Added segment MRG

---
+++ test/test_mrg_segment.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+require File.dirname(__FILE__) + '/test_helper'
+
+class MrgSegment < Test::Unit::TestCase
+  def setup
+    @base = "MRG|0533132^^^CARY^MR"
+  end
+
+  def test_prior_patient_id
+    mrg_str = "MRG|0533132^^^CARY^MR"
+    mrg = HL7::Message::Segment::MRG.new(mrg_str)
+    
+    prior_patient_id = mrg.prior_patient_id
+    assert prior_patient_id.is_a?(Hash)
+    assert_equal '0533132', prior_patient_id['id']
+  end
+
+  def test_prior_patient_id_without_component
+    mrg_str = "MRG|0533132"
+    mrg = HL7::Message::Segment::MRG.new(mrg_str)
+    
+    prior_patient_id = mrg.prior_patient_id
+    assert prior_patient_id.is_a?(String)
+    assert_equal '0533132', prior_patient_id
+  end
+end
