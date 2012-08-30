4506b230d3133ff2abadf77d8fd72469745a7b04
Jon Kern <jonkern@comcast.net>
2012-08-30 15:09:49 -0400

    Added zed name for 2nd field (hospital unit)

--- lib/segments/zed.rb
+++ lib/segments/zed.rb
@@ -3,6 +3,6 @@
 #   ZED|ABSCESS^Abscess|3C1-12 Cardiology|11108232
 class HL7::Message::Segment::ZED < HL7::Message::Segment
   add_field :chief_complaint
-  add_field :unknown2
+  add_field :hospital_unit
   add_field :armband_id
 end
--- test/test_zed_segment.rb
+++ test/test_zed_segment.rb
@@ -2,13 +2,13 @@ require File.dirname(__FILE__) + '/test_helper'
 class ZedSegment < Test::Unit::TestCase
 
   def setup
-    @base = "ZED|CARDIAC^Cardiac Symptoms||246315"
+    @base = "ZED|CARDIAC^Cardiac Symptoms|4E-Pediatrics|246315"
   end
 
   def test_initial_read
     zed = HL7::Message::Segment::ZED.new @base
     assert_equal( "CARDIAC^Cardiac Symptoms", zed.chief_complaint )
-    assert_equal( "", zed.unknown2 )
+    assert_equal( "4E-Pediatrics", zed.hospital_unit )
     assert_equal( "246315", zed.armband_id )
   end
 
