18d9bb33bda73fffccdfe58d84f27dc7239d841e
Guangming Cui <gcui@axialexchange.com>
2014-06-14 09:59:07 -0400

    Added to message type test: ORM^O01

--- test/test_message_types.rb
+++ test/test_message_types.rb
@@ -30,4 +30,17 @@ class MessageTypes < Test::Unit::TestCase
     assert_equal('MDM', msh.message_type['message_type'])
     assert_equal('T02', msh.message_type['trigger_event'])
   end
+
+  def test_orm_o01
+    msg = TestHelper.load_hl7(:orm_o01).first
+    hl7_msg = HL7::Message.new
+    hl7_msg.parse msg
+
+    msh = hl7_msg.segments.select do |s|
+            s.is_a? HL7::Message::Segment::MSH
+          end.first
+
+    assert_equal('ORM', msh.message_type['message_type'])
+    assert_equal('O01', msh.message_type['trigger_event'])
+  end
 end
